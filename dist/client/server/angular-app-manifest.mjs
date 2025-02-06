c221fb0db97f584907e2a4f30ad1f53ba15408c2
yaelajami2 <y0556722091@gmail.com>
2025-02-06 12:28:45 +0200

    banner

--- dist/client/server/angular-app-manifest.mjs
+++ dist/client/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5738, hash: '76b0544b9a42074ed25a265d4acceb7bd7dd266937f8efc108729df63609ae60', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3865, hash: '6dff73f3dba2743d34bb06ede40da1cf4a02b823359c51a8bcb808d95b5376a4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-LMSVF625.css': {size: 129002, hash: 'EwW92iZYOlo', text: () => import('./assets-chunks/styles-LMSVF625_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5753, hash: '3909a7523b6d03b61a6014f993ec9f3fca6c0a5d17cc0e96ce5aec25d039aa60', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3865, hash: '6b45d6bb0e88712b8dae0e0a96dca7c39aa860577c8d4d1d8d4adbe19704e363', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-RDOBM6CR.css': {size: 129107, hash: 'hkpwe4JULiQ', text: () => import('./assets-chunks/styles-RDOBM6CR_css.mjs').then(m => m.default)}
   },
 };
