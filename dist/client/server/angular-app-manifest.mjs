6f1cd835dfab7d7b02648b27ffeefc5653d0177d
yaelajami2 <y0556722091@gmail.com>
2025-03-05 11:36:55 +0200

    hhhh

--- dist/client/server/angular-app-manifest.mjs
+++ dist/client/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5833, hash: 'bcd6897264e56345fcb43196e5ea6feda2f86757f07c306067c547386d2a8ca2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3865, hash: '9502f8d6432e2f09c7216a1d4d797ba0cdcb0c2d2ccbe7b5820e275a36b88921', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-JHBENX5W.css': {size: 129233, hash: 'jqZB+Um1lHc', text: () => import('./assets-chunks/styles-JHBENX5W_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5833, hash: 'd910042a164b9afc0abe8e13eb18044cadcfd5d7281df25cd8209a46ec94e1e4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3865, hash: 'a94ecc65b3ccbfb6dd681ed306ad42723cb2d882c1e54ec595f20cb56c605255', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-FJARS4A3.css': {size: 129233, hash: 'p6html27Zj8', text: () => import('./assets-chunks/styles-FJARS4A3_css.mjs').then(m => m.default)}
   },
 };
