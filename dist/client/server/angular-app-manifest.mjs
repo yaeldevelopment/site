bef75c212c833aa358388b74d368cda31322b687
yaelajami2 <y0556722091@gmail.com>
2025-06-06 15:18:33 +0300

    about page

--- dist/client/server/angular-app-manifest.mjs
+++ dist/client/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5833, hash: '9aa6c4ca1aaaf09e3e7ff83f464d6ef85711ec43cb2c7c8eda1eb0b094f41ccb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3865, hash: '1f6985e47ffd1796357feb3fea5e7df76d5f3dd72a35a1886b0afe7b420d0b11', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5833, hash: '569f43d28c3303d37163daf78bc4e5178e33267e804d31efea4d4b7ec2365f2c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3865, hash: '7fcf18540d051c67d66dd2c9c25c06572007be481127522ddee36e3f05a1bfff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-W22ZBBX4.css': {size: 129346, hash: 'BJ9EBya/vQ8', text: () => import('./assets-chunks/styles-W22ZBBX4_css.mjs').then(m => m.default)}
   },
 };
